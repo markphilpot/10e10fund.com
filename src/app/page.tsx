7984874f97c6468ceeb53510c07b35816ae18b20
Mark Philpot <mark.philpot@gmail.com>
2025-09-14 16:59:48 -0700

    Tweak mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,19 @@ export default function Home() {
     <div
       className={"font-sans h-screen w-screen grid grid-cols-12 grid-rows-12"}
     >
-      <div className={"row-start-4 col-start-4 text-4xl tracking-normal"}>
+      <div
+        className={
+          "row-start-3 col-start-3 md:row-start-4 md:col-start-4 text-4xl tracking-normal"
+        }
+      >
         10e10
         <BlinkingCursor />
       </div>
-      <div className={"row-start-11 col-start-8 col-span-3"}>
+      <div
+        className={
+          "row-start-8 col-start-8 md:row-start-11 md:col-start-8 col-span-3"
+        }
+      >
         for exponential builders
       </div>
     </div>
